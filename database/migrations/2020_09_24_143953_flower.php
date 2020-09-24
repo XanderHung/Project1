459487c5e560edf0598d44fef3e11e5268bf1f67
Christian Adiputra <adiputrachristian17@gmail.com>
2020-09-24 22:04:33 +0700

    Tambahan db category dan flower

---
+++ database/migrations/2020_09_24_143953_flower.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Flower extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flower', function (Blueprint $table) {
+            $table->bigIncrements('flowerid');
+            $table->integer('categoryid')->unsigned();
+            $table->foreign('categoryid')->references('categoryid')->on('category');
+            $table->string('flowername',50);
+            $table->integer('price');
+            $table->text('description');
+            $table->mediumtext('flowerimage')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flower');
+    }
+}
