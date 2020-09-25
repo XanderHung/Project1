06b8745fa39fc74b50fc74963853d1e2cf775815
XanderHung <alexander.hung@binus.ac,id>
2020-09-25 23:19:57 +0700

    new update view

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -43,9 +43,29 @@
     </div>
 </nav>
 <div class="container">
-    <h1>Welcome To Flowelto Shop</h1>
-    <h3>The best Flower Shop in Binus University</h3>
-    
+    <h1 class="col-xs-4 col-xs-offset-4">Login</h1>
+        <form class="px-4 py-3">
+            <div class="form-group">
+                <label for="exampleDropdownFormEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleDropdownFormEmail1" placeholder="email@example.com">
+            </div>
+            <div class="form-group">
+                <label for="exampleDropdownFormPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleDropdownFormPassword1" placeholder="Password">
+            </div>
+            <div class="form-group">
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="dropdownCheck">
+                    <label class="form-check-label" for="dropdownCheck">
+                    Remember me
+                    </label>
+                </div>
+            </div>
+        <button type="submit" class="btn btn-primary">Sign in</button>
+        </form>
+    <div class="dropdown-divider"></div>
+        <a class="dropdown-item" href="#">New around here? Sign up</a>
+        <a class="dropdown-item" href="#">Forgot password?</a>
     </div>
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
