4fe0b1c2355c79b774b1a090287772abf00a69f8
XanderHung <alexander.hung@binus.ac,id>
2020-10-02 11:25:52 +0700

    new view

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', 'UserController@index');
-Route::get('/category', 'AdminController@addcategory');
+Route::get('/addcat', 'AdminController@addcategory');
 Route::post('/admin/create', 'AdminController@create');
-Route::get('/viewcategory','AdminController@viewcategory');
+Route::get('/viewcat','AdminController@viewcategory');
