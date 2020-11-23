eb63624c5d2b6aa2f361d8581a41d42ea2e7797c
XanderHung <alexander.hung@binus.ac,id>
2020-11-23 21:36:31 +0700

    update home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,8 @@
 @extends('viewdef')
 
 @section('content')
-<div class="container text-center">
-    <div class="m-4">
+<div class="row col-md-12 text-center">
+    <div class="col">
         <h1>Welcome To Flowelto Shop</h1>
         <h3>The best Flower Shop in Binus University</h3>
     </div>
